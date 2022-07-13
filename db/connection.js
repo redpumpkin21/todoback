292fb4561de01b6dd513e64d0f14c35681bcd9bd
redpumpkin21 <christopher.johnson2108@gmail.com>
2022-07-13 12:52:32 -0400

    dotenv

---
+++ db/connection.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+const MONGODB_URI = process.env.MONGODB_URI
+
+const config = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}
+
+mongoose.connect(MONGODB_URI, config)
+
+mongoose.connection 
+    .on('open', () => console.log('MONGO CONNECTION IS OPEN'))
+    .on('CLOSE', () => console.log('MONGO CONNECTION IS CLOSED'))
+    .on('error', (error) => console.log('MONGO CONNECTION ERROR \n***********\n', error))
+
+
+module.exports = mongoose
--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 require('custom-env').env(true)
 
+const PORT = process.env.PORT
 
 const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
+const db = mongoose.connection
 
 const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(moragn('tiny'))
+mongoose.connect('')
