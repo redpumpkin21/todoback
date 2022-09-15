46c3b1d3696e5782237f13fd40132ef326aaf308
redpumpkin21 <christopher.johnson2108@gmail.com>
2022-09-14 21:55:47 -0400

    umhow

---
+++ models/User.js
@@ -0,0 +1,25 @@
+// const mongoose = requir('mongoose')
+
+// const UserSchema = new Schema({
+//     username: {
+//         type: String,
+//         required: true,
+
+//     },
+//     email: {
+//         type: String,
+//         required: true
+//     },
+//     password:{
+//         type: String,
+//         required: true
+//     },
+//     createdAt:{
+//         type: Date,
+//         default: Date.now()
+//     },
+//     token: {type: String}
+
+// })
+
+// module.exports = User
